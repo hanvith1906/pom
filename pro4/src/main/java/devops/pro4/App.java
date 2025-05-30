f5d9659631e65224492b761acd1f9d3edbcc180d
hanvith1906 <hanvith.2004@gmail.com>
2025-05-31 00:19:38 +0530

    Add files via upload

---
+++ pro4/src/main/java/devops/pro4/App.java
@@ -0,0 +1,13 @@
+package devops.pro4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
